051d3b1fb889ed3d807b70db7dac3eafeba9ec06
Chakshing Lam <chakshinglam@gmail.com>
2022-09-29 23:54:02 -0400

    added more detail to the pages router  animation

--- src/js/usePageAnimation.js
+++ src/js/usePageAnimation.js
@@ -3,15 +3,31 @@ export default function(){
     $('#main-page').on('pagebeforehide',function(){
 
         var theHeight = $(this).find('#animation-default').css('height')
-        
-        $(this).find('#animation-default').animate({bottom:'-'+ theHeight},300)
 
-        
+        var theHigherHeight = $(this).find('#animation-default').outerHeight() + 10 +"px"
+
+        //div stretch
+        $(this).find('#animation-default').animate({height: theHigherHeight},200)
+        $(this).find('#animation-default').animate({height: theHeight},100)
+
+        //div slideout
+        $(this).find('#animation-default').animate({bottom:'-'+ theHeight},300)
+    
     })
 
     $('#main-page').on('pageshow',function(){
 
+        var theHeight = $(this).find('#animation-default').css('height')
+
+        var theHigherHeight = $(this).find('#animation-default').outerHeight() + 10 +"px"
+ 
+        //div slidein
         $(this).find('#animation-default').animate({bottom:'0%'},300)
+
+        //div stretch
+        $(this).find('#animation-default').animate({height: theHigherHeight},100)
+        $(this).find('#animation-default').animate({height: theHeight},200)
+        
         
     })
 
