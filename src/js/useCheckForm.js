877153425e7881e5a21cf53355b7b3405b792f2a
Chakshing Lam <chakshinglam@gmail.com>
2022-10-06 23:41:36 -0400

    add active lock to signin form check

--- src/js/useCheckForm.js
+++ src/js/useCheckForm.js
@@ -3,22 +3,30 @@ function checkSigninForm(){
     const userval = $("#signin-username").val()
     const passval = $("#signin-password").val()
 
-    console.log(userval, passval)
+    let loginActiveLock = false //行为锁
 
-    if (userval === "user" && passval === "pass") {// Logged In
+    if(!loginActiveLock){
 
-        console.log("Success")
-        sessionStorage.userId = 3//存入证书
-        $("#signin-form")[0].reset()//清空input
+        if (userval === "user" && passval === "pass") {// Logged In
 
-    } else {// Not Logged In
+            loginActiveLock = true //锁定
+            console.log("Success")
+            sessionStorage.userId = 3//存入证书
+            $("#signin-form")[0].reset()//清空input
 
-        console.log("Failure")
-        sessionStorage.removeItem("userId")//清空证书
-        $("#signin-form")[0].reset()//清空input
-        $("#signInPageError_slot").html("Incorrect user name or password").css('color','var(--color-font-alert)')//插入错误警告
-        setTimeout(()=>{$("#signInPageError_slot").html("Hi! Welcome to Color Cats").css('color','var(--color-font-light-v)');},3000)//移除错误警告
-    
+            setTimeout(()=>{loginActiveLock = false},3000)//登录成功后三秒后解锁
+
+        } else {// Not Logged In
+
+            console.log("Failure")
+            sessionStorage.removeItem("userId")//清空证书
+            $("#signin-form")[0].reset()//清空input
+            $("#signInPageError_slot").html("Incorrect user name or password").css('color','var(--color-font-alert)')//插入错误警告
+            setTimeout(()=>{$("#signInPageError_slot").html("Hi! Welcome to Color Cats").css('color','var(--color-font-light-v)');},3000)//移除错误警告
+        
+        }
+    }else{
+        return
     }
 }
 
