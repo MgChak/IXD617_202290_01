312ccf3c0d8ea5412697b7eee17a7b5b1e82096d
Chakshing Lam <chakshinglam@gmail.com>
2022-11-02 19:45:07 -0400

    change stars input data range from 0-4 to 1-5

--- src/js/useAddingEditPageFunctions.js
+++ src/js/useAddingEditPageFunctions.js
@@ -5,9 +5,11 @@ function starsCtrl(tar){
 
     $(tar).nextAll().attr("src","./src/img/icons/starUnactive.svg")
 
-    var tarIndex = $(tar).index()//获取目标序列号
+    var tarIndex = $(tar).index() + 1//获取目标序列号
 
     $(tar).parent().siblings('input').val(tarIndex) //把选中目标的序列号赋予input
+
+    console.log(tarIndex)
 
 }
 
